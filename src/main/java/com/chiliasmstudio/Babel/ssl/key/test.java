22c93936e26d3cc302ddd251876144bd61a32a14
paul <paul002706@gmail.com>
2023-03-20 05:35:42 +0800

    Brrrrrrrrrrt

--- src/main/java/com/chiliasmstudio/Babel/ssl/key/test.java
+++ src/main/java/com/chiliasmstudio/Babel/ssl/key/test.java
@@ -13,6 +13,7 @@ import org.bouncycastle.operator.ContentSigner;
 import org.bouncycastle.operator.DefaultDigestAlgorithmIdentifierFinder;
 import org.bouncycastle.operator.DefaultSignatureAlgorithmIdentifierFinder;
 import org.bouncycastle.operator.bc.BcRSAContentSignerBuilder;
+import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
 
 import java.io.FileOutputStream;
 import java.math.BigInteger;
@@ -21,123 +22,83 @@ import java.security.KeyPairGenerator;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.SecureRandom;
+import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 public class test {
     public static void main(String[] args) throws Exception {
-        // Generate root certificate
+        // Generate root key pair
         KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
         keyPairGenerator.initialize(2048, new SecureRandom());
         KeyPair rootKeyPair = keyPairGenerator.generateKeyPair();
-        PrivateKey rootPrivateKey = rootKeyPair.getPrivate();
         PublicKey rootPublicKey = rootKeyPair.getPublic();
+        PrivateKey rootPrivateKey = rootKeyPair.getPrivate();
 
-        X500Name rootIssuer = new X500Name("CN=Root");
-        BigInteger rootSerial = BigInteger.valueOf(new SecureRandom().nextInt());
+        // Generate intermediate key pair
+        KeyPair intermediateKeyPair = keyPairGenerator.generateKeyPair();
+        PublicKey intermediatePublicKey = intermediateKeyPair.getPublic();
+        PrivateKey intermediatePrivateKey = intermediateKeyPair.getPrivate();
 
-        Date notBefore = new Date();
-        Date notAfter = new Date(notBefore.getTime() + 365 * 24 * 60 * 60 * 1000L);
+        // Generate end entity key pair
+        KeyPair endEntityKeyPair = keyPairGenerator.generateKeyPair();
+        PublicKey endEntityPublicKey = endEntityKeyPair.getPublic();
+        PrivateKey endEntityPrivateKey = endEntityKeyPair.getPrivate();
 
+        // Generate root certificate
         X509v3CertificateBuilder rootCertBuilder = new X509v3CertificateBuilder(
-                rootIssuer,
-                rootSerial,
-                notBefore,
-                notAfter,
-                rootIssuer,
+                new X500Name("CN=Root"),
+                BigInteger.valueOf(1),
+                new Date(System.currentTimeMillis() - 1000L * 60 * 60 * 24),
+                new Date(System.currentTimeMillis() + (1000L * 60 * 60 * 24 * 365)),
+                new X500Name("CN=Root"),
                 SubjectPublicKeyInfo.getInstance(rootPublicKey.getEncoded())
         );
-
-        rootCertBuilder.addExtension(
-                Extension.basicConstraints,
-                true,
-                new BasicConstraints(true)
-        );
-
-        rootCertBuilder.addExtension(
-                Extension.keyUsage,
-                true,
-                new KeyUsage(KeyUsage.keyCertSign | KeyUsage.cRLSign)
-        );
-
-        ContentSigner rootContentSigner = new BcRSAContentSignerBuilder(
-                new DefaultSignatureAlgorithmIdentifierFinder().find("SHA256withRSA"),
-                new DefaultDigestAlgorithmIdentifierFinder().find("SHA-256")
-        ).build(PrivateKeyFactory.createKey(rootPrivateKey.getEncoded()));
-
+        ContentSigner rootContentSigner = new JcaContentSignerBuilder("SHA256WithRSAEncryption").build(rootPrivateKey);
         X509CertificateHolder rootCertHolder = rootCertBuilder.build(rootContentSigner);
         X509Certificate rootCert = new JcaX509CertificateConverter().getCertificate(rootCertHolder);
 
-
         // Generate intermediate certificate
-        KeyPair intermediateKeyPair = keyPairGenerator.generateKeyPair();
-        PrivateKey intermediatePrivateKey = intermediateKeyPair.getPrivate();
-        PublicKey intermediatePublicKey = intermediateKeyPair.getPublic();
-
-        X500Name intermediateIssuer = new X500Name("CN=Intermediate");
-        BigInteger intermediateSerial = BigInteger.valueOf(new SecureRandom().nextInt());
-
         X509v3CertificateBuilder intermediateCertBuilder = new X509v3CertificateBuilder(
-                rootIssuer,
-                intermediateSerial,
-                notBefore,
-                notAfter,
-                intermediateIssuer,
-                SubjectPublicKeyInfo.getInstance(intermediatePublicKey.getEncoded())
-        );
-
-        intermediateCertBuilder.addExtension(
-                Extension.basicConstraints,
-                true,
-                new BasicConstraints(true)
-        );
-
-        intermediateCertBuilder.addExtension(
-                Extension.keyUsage,
-                true,
-                new KeyUsage(KeyUsage.keyCertSign | KeyUsage.cRLSign)
-        );
-
-        ContentSigner intermediateContentSigner = new BcRSAContentSignerBuilder(
-                new DefaultSignatureAlgorithmIdentifierFinder().find("SHA256withRSA"),
-                new DefaultDigestAlgorithmIdentifierFinder().find("SHA-256")
-        ).build(PrivateKeyFactory.createKey(rootPrivateKey.getEncoded()));
-
+                new X500Name("CN=Root"),
+                BigInteger.valueOf(2),
+                new Date(System.currentTimeMillis() - 1000L * 60 * 60 * 24),
+                new Date(System.currentTimeMillis() + (1000L * 60 * 60 * 24 * 365)),
+                new X500Name("CN=Intermediate"),
+                SubjectPublicKeyInfo.getInstance(intermediatePublicKey.getEncoded()));
+        ContentSigner intermediateContentSigner = new JcaContentSignerBuilder("SHA256WithRSAEncryption").build(rootPrivateKey);
         X509CertificateHolder intermediateCertHolder = intermediateCertBuilder.build(intermediateContentSigner);
         X509Certificate intermediateCert = new JcaX509CertificateConverter().getCertificate(intermediateCertHolder);
 
-        // Generate end certificate
-        KeyPair endKeyPair = keyPairGenerator.generateKeyPair();
-        PrivateKey endPrivateKey = endKeyPair.getPrivate();
-        PublicKey endPublicKey = endKeyPair.getPublic();
-
-        X500Name endIssuer = new X500Name("CN=End");
-        BigInteger endSerial = BigInteger.valueOf(new SecureRandom().nextInt());
-
-        X509v3CertificateBuilder endCertBuilder = new X509v3CertificateBuilder(
-                intermediateIssuer,
-                endSerial,
-                notBefore,
-                notAfter,
-                endIssuer,
-                SubjectPublicKeyInfo.getInstance(endPublicKey.getEncoded())
+        // Generate end entity certificate
+        X509v3CertificateBuilder endEntityCertBuilder = new X509v3CertificateBuilder(
+                new X500Name("CN=Intermediate"),
+                BigInteger.valueOf(3),
+                new Date(System.currentTimeMillis() - 1000L * 60 * 60 * 24),
+                new Date(System.currentTimeMillis() + (1000L * 60 * 60 * 24 * 365)),
+                new X500Name("CN=End Entity"),
+                SubjectPublicKeyInfo.getInstance(endEntityPublicKey.getEncoded())
         );
+        ContentSigner endEntityContentSigner = new JcaContentSignerBuilder("SHA256WithRSAEncryption").build(intermediatePrivateKey);
+        X509CertificateHolder endEntityCertHolder = endEntityCertBuilder.build(endEntityContentSigner);
+        X509Certificate endEntityCert = new JcaX509CertificateConverter().getCertificate(endEntityCertHolder);
 
-        endCertBuilder.addExtension(
-                Extension.basicConstraints,
-                true,
-                new BasicConstraints(false)
-        );
+        // Create certificate chain
+        CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
+        List<X509Certificate> certChain = Arrays.asList(endEntityCert, intermediateCert, rootCert);
 
-        ContentSigner endContentSigner = new BcRSAContentSignerBuilder(
-                new DefaultSignatureAlgorithmIdentifierFinder().find("SHA256withRSA"),
-                new DefaultDigestAlgorithmIdentifierFinder().find("SHA-256")
-        ).build(PrivateKeyFactory.createKey(intermediatePrivateKey.getEncoded()));
 
-        X509CertificateHolder endCertHolder = endCertBuilder.build(endContentSigner);
-        X509Certificate endCert = new JcaX509CertificateConverter().getCertificate(endCertHolder);
 
-        // TODO: Save certificates in desired formats (Java KeyStore, CRT, PFX)
+
+
+
+
+
+
+
+
 
         // Save root certificate as CRT
         try (FileOutputStream fos = new FileOutputStream("root.crt")) {
@@ -151,7 +112,7 @@ public class test {
 
 // Save end certificate as CRT
         try (FileOutputStream fos = new FileOutputStream("end.crt")) {
-            fos.write(endCert.getEncoded());
+            fos.write(endEntityCert.getEncoded());
         }
     }
 }
